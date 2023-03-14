d28da8e403391b309ede83edede01cc8ad069077
LINSTCL <linstcl@qq.com>
2023-03-14 09:48:21 +0800

    *

---
+++ src/build.py
@@ -0,0 +1,10 @@
+from .fabirc import Neure
+
+class NeureGroup():
+
+    neures = [Neure]
+
+    def __init__(self):
+        pass
+
+
--- src/fabirc.py
+++ src/fabirc.py
@@ -1,10 +1,15 @@
 
 
-class Neure():
-    def __init__(self, in_port_num, out_port_num, 
-                 disseminate_fun, internal:bool, prev_neure=None):
-        self.in_port_num = in_port_num
-        self.out_port_num = out_port_num
-        self.disseminate_fun = disseminate_fun
 
+class Neure():
+    def __init__(self, in_port, out_port, 
+                 disseminate_fun, value):
+        self.in_port = in_port
+        self.out_port = out_port
+        self.disseminate_fun = disseminate_fun
+        self.value = value
+
+    def run(self):
+        self.value = self.disseminate_fun(self.in_port.value)
+        return self.out_port
 
